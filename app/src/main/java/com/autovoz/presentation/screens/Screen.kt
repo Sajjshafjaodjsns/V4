a4a1fa77335dd2ebf1b6cbe1955e5c5465d61fe2
Sajjshafjaodjsns <05turbo25@gmail.com>
2026-04-18 00:15:22 +0300

    Initial commit

---
+++ app/src/main/java/com/autovoz/presentation/screens/Screen.kt
@@ -0,0 +1,9 @@
+package com.autovoz.presentation.screens
+
+sealed class Screen(val route: String) {
+    object Trips : Screen("trips")
+    object NewTrip : Screen("new_trip")
+    object Result : Screen("result")
+    object Settings : Screen("settings")
+    object Library : Screen("library")
+}
